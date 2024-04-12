d82f48b8603c2211b9683876d9b450a13fabe4a5
JuanValeraDev <juanvaleradev@gmail.com>
2024-04-12 19:28:50 +0200

    He añadido un nuevo script que pone a prueba la capacidad de un modelo de mantener la esencia de un texto a pesar de tener mayor o menor tamaño.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 Esto es un proyecto que tiene algunos scripts de python en los que se practica un poco con modelos de inteligencia artificial ejecutados con ficheros llamafile. 
 
-El script más llamativo es market_test_pilot_product_speaker.py. 
+Los scripts más llamativos son market_test_pilot_product_speaker.py y story_summary_reconstruction.
+
+- market_test_pilot_product_speaker.py:
+
 En este script se presenta una situación imaginaria en la que una compañía ha sacado un producto piloto de un altavoz y quiere saber la opinión de los consumidores. 
 Tenemos una base de datos local mysql con 10 reseñas sobre el altavoz. Tenemos cuatro topics que aparecen de forma salteada en las reseñas: design, shipping, price, maxVolume (una nueva feature).
 
@@ -11,3 +14,9 @@ Una vez hecho este proceso, se le pasa al modelo todas las respuestas generadas 
 Esto puede ser muy útil como análisis de mercado para conocer la recepción de un producto piloto entre los consumidores. 
 
 Es interesante ejecutarlo con distintos modelos para ver qué resultado nos da cada uno.
+
+- story_summary_reconstruction:
+
+Este script le pide al modelo que genere una historia de entre 100 y 120 palabras. Después le pide que la resuma a 50-70 palabras. Seguidamente le pide que reconstruya la historia original usando entre 150 y 180 palabras. A continuación vuelve a pedirle que la resuma, esta vez usando entre 20 y 40 palabras. Finalmente le pide una última reconstrucción de 200 a 220 palabras. 
+
+Con este script he querido investigar hasta qué punto los modelos pueden comprimir y expandir el tamaño de un texto manteniendo su sentido y significado. Es muy interesante ver los resultados utilizando distintos modelos y distintas temperaturas. Este script sirve para testar diferentes modelos y ver su capacidad de síntesis y análisis de un texto.
--- story_summary_reconstruction.py
+++ story_summary_reconstruction.py
@@ -1,8 +1,10 @@
 from openai import OpenAI
 import mysql.connector
 
-model = "Rocket-3B"
+model = "Rocket 3-B"
 temp = 1.0
+print("\nModelo: ", model)
+print("\nTemperatura: ", temp)
 
 conexion = mysql.connector.connect(host="localhost", user="juan", password="mysqljuan", database="llamafiles")
 cursor = conexion.cursor()
@@ -11,33 +13,62 @@ client = OpenAI(
     base_url="http://127.0.0.1:8080/v1",
     api_key="sk-no-key-required"
 )
-completion = client.chat.completions.create(
+first_completion = client.chat.completions.create(
     model="LLaMA_CPP",
     messages=[
-        {"role": "system", "content": "I want you to invent a story about cats and dogs."
-                                      "I want it to be between 100 and 200 words long."},
+        {"role": "system", "content": "I want you to invent a story between 100 and 120 words long."},
     ],
     temperature=temp,
 )
 
-original_story = completion.choices[0].message.content
+original_story = first_completion.choices[0].message.content
 print("Historia original: \n", original_story)
 
-new_message = {"role": "user", "content": f"I want you to summarize the following story: {original_story}"}
-
-messages = [
-    {"role": "system", "content": "You are a summarization assistant."},
-    new_message
-]
-temp = 0.0
 summary_completion = client.chat.completions.create(
     model="LLaMA_CPP",
-    messages=messages,
+    messages=[
+        {"role": "system", "content": "You are a summarization assistant."},
+        {"role": "user",
+         "content": f"I want you to summarize in 50-70 words the following story: {original_story}"}],
     temperature=temp,
 )
 
+first_summary = summary_completion.choices[0].message.content
 print("Resumen: \n", summary_completion.choices[0].message.content)
 
-cursor.execute("INSERT INTO story_summary_reconstruction (model, original_story) VALUES (%s, %s)",
-               (model, completion.choices[0].message.content))
+first_reconstruction_completion = client.chat.completions.create(
+    model="LLaMA_CPP",
+    messages=[
+        {"role": "user", "content": f"Imagine the original 150-180 word story from this summary: {first_summary}"}],
+    temperature=temp,
+)
+first_reconstruction = first_reconstruction_completion.choices[0].message.content
+print("Reconstrucción: \n", first_reconstruction)
+
+second_summary_completion = client.chat.completions.create(
+    model="LLaMA_CPP",
+    messages=[
+        {"role": "system", "content": "You are a summarization assistant."},
+        {"role": "user",
+         "content": f"I want you to summarize in 20-40 words the following story: {first_reconstruction}"}],
+    temperature=temp,
+)
+
+second_summary = second_summary_completion.choices[0].message.content
+print("Segundo resumen: \n", second_summary)
+
+second_reconstruction_completion = client.chat.completions.create(
+    model="LLaMA_CPP",
+    messages=[
+        {"role": "user", "content": f"Imagine the original 200-220 word story from this summary: {first_summary}"}],
+    temperature=temp,
+)
+second_reconstruction = second_reconstruction_completion.choices[0].message.content
+print("Segunda reconstrucción: \n", second_reconstruction)
+
+cursor.execute(
+    "INSERT INTO story_summary_reconstruction (model, temp, original_story, first_summary, first_reconstruction, "
+    "second_summary, second_reconstruction) VALUES ("
+    "%s, %s, %s, %s, %s, %s, %s)",
+    (model, temp, original_story, first_summary, first_reconstruction, second_summary, second_reconstruction))
 conexion.commit()
